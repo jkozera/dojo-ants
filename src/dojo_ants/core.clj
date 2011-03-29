34fbc29fea1a72c277e45fe807c32f14c951cb78
Jerzy Kozera <jerzy.kozera@gmail.com>
2011-03-29 20:13:48 +0100

    Frist!!!!!!!!!!!1

--- src/dojo_ants/core.clj
+++ src/dojo_ants/core.clj
@@ -1,1 +1,12 @@
 (ns dojo-ants.core)
+
+(println "Hello World!")
+
+(def antworld {:ourants [[7 9]] :food [[6 5]]})
+
+(defn nearest-food [antWorld] 
+  (first (antWorld :food))
+  )
+
+(defn move-first-ant-to [world location]
+  (update-in world [:ourants] (fn [x] location)))
