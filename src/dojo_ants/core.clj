4c43fae5b7a008dc1c9c53806fd776428457be98
Jerzy Kozera <jerzy.kozera@gmail.com>
2011-03-29 20:31:31 +0100

    Nearest food now works

--- src/dojo_ants/core.clj
+++ src/dojo_ants/core.clj
@@ -2,10 +2,17 @@
 
 (println "Hello World!")
 
-(def antworld {:ourants [[7 9]] :food [[6 5]]})
+(def antworld {:ourants [[7 9]] :food [[6 5][7 10][7 11][9999 9999]]})
 
+(defn distance [a b]
+  (let [[x1 y1] a [x2 y2] b]
+  (+ (* (- x1 x2) (- x1 x2))
+     (* (- y1 y2) (- y1 y2)))))
+
+
+  
 (defn nearest-food [antWorld] 
-  (first (antWorld :food))
+  (first (sort-by (fn [food] (distance food [7 9]) ) (antWorld :food)))
   )
 
 (defn move-first-ant-to [world location]
