920d5193c94ff2acd6050282be7025b6678a3767
Jerzy Kozera <jerzy.kozera@gmail.com>
2011-03-29 20:52:38 +0100

    Added path calculation

--- src/dojo_ants/core.clj
+++ src/dojo_ants/core.clj
@@ -2,14 +2,42 @@
 
 (println "Hello World!")
 
-(def antworld {:ourants [[7 9]] :food [[6 5][7 10][7 11][9999 9999]]})
+(def antworld {:ourants [[7 9]] :food [[6 5][7 11][9999 9999]]})
 
 (defn distance [a b]
   (let [[x1 y1] a [x2 y2] b]
   (+ (* (- x1 x2) (- x1 x2))
      (* (- y1 y2) (- y1 y2)))))
 
+(defn move-closer [source destination]
+  (let [[x1 y1] source
+        [x2 y2] destination
+        dx (- x1 x2)
+        dy (- y1 y2)]
+    (if (= dx 0)
+      (if (= dy 0)
+        destination
+        (if (> dy 0)
+          [x1 (dec y1)]
+          [x1 (inc y1)])
 
+        )
+      (if (> dx 0)
+        [(dec x1) y1]
+        [(inc x1) y1])
+      )
+    )
+  )
+
+(defn path [world source destination]
+  (if (= source destination)
+    [source]
+    (let [next-step  (move-closer source destination)]
+      (cons next-step (path world next-step destination))
+    )
+  
+  )
+  )
   
 (defn nearest-food [antWorld] 
   (first (sort-by (fn [food] (distance food [7 9]) ) (antWorld :food)))
